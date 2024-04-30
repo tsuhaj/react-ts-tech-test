ca164707c60ccd19b329539051a95b2503eacb80
Tomáš Šuhaj <t.suhaj@gmail.com>
2024-04-30 12:16:12 +0200

    Task 1 - Increased table row font size to 20px

--- src/components/EstablishmentsTableRow.tsx
+++ src/components/EstablishmentsTableRow.tsx
@@ -1,10 +1,14 @@
+const tableRowStyle: { [key: string]: string | number } = {
+	fontSize: "20px",
+};
+
 export const EstablishmentsTableRow: React.FC<{
-  establishment: { [key: string]: string } | null | undefined;
+	establishment: { [key: string]: string } | null | undefined;
 }> = ({ establishment }) => {
-  return (
-    <tr>
-      <td>{establishment?.BusinessName}</td>
-      <td>{establishment?.RatingValue}</td>
-    </tr>
-  );
+	return (
+		<tr style={tableRowStyle}>
+			<td>{establishment?.BusinessName}</td>
+			<td>{establishment?.RatingValue}</td>
+		</tr>
+	);
 };
