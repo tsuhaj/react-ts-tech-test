9b746f6c2c727601d861ef1b6bb701431da7d867
Tomáš Šuhaj <t.suhaj@gmail.com>
2024-04-30 12:17:50 +0200

    Task 2 - Added loading text when fetching new data

--- src/components/EstablishmentsTable.tsx
+++ src/components/EstablishmentsTable.tsx
@@ -3,38 +3,40 @@ import { EstablishmentsTableRow } from "./EstablishmentsTableRow";
 import PropTypes from "prop-types";
 
 const headerStyle: { [key: string]: string | number } = {
-  paddingBottom: "10px",
-  textAlign: "left",
-  fontSize: "20px",
+	paddingBottom: "10px",
+	textAlign: "left",
+	fontSize: "20px",
 };
 
 export const EstablishmentsTable: React.FC<{
-  establishments: { [key: string]: string }[] | null | undefined;
-}> = ({ establishments }) => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <th style={headerStyle}>Business Name</th>
-          <th style={headerStyle}>Rating Value</th>
-        </tr>
-        {establishments &&
-          establishments?.map(
-            (
-              establishment: { [key: string]: string } | null | undefined,
-              index: React.Key | null | undefined
-            ) => (
-              <EstablishmentsTableRow
-                key={index}
-                establishment={establishment}
-              />
-            )
-          )}
-      </tbody>
-    </table>
-  );
+	establishments: { [key: string]: string }[] | null | undefined;
+	loading: boolean;
+}> = ({ establishments, loading }) => {
+	return (
+		<table>
+			<tbody>
+				<tr>
+					<th style={headerStyle}>Business Name</th>
+					<th style={headerStyle}>Rating Value</th>
+				</tr>
+
+				{loading ? (
+					<tr>
+						<td>Loading...</td>
+					</tr>
+				) : (
+					establishments &&
+					establishments?.map(
+						(establishment: { [key: string]: string } | null | undefined, index: React.Key | null | undefined) => (
+							<EstablishmentsTableRow key={index} establishment={establishment} />
+						)
+					)
+				)}
+			</tbody>
+		</table>
+	);
 };
 
 EstablishmentsTable.propTypes = {
-  establishments: PropTypes.array,
+	establishments: PropTypes.array,
 };
--- src/components/PaginatedEstablishmentsTable.tsx
+++ src/components/PaginatedEstablishmentsTable.tsx
@@ -4,72 +4,75 @@ import { EstablishmentsTableNavigation } from "./EstablishmentsTableNavigation";
 import { getEstablishmentRatings } from "../api/ratingsAPI";
 
 const tableStyle = {
-  background: "#82C7AF",
-  padding: "10px",
-  width: "max-content",
-  marginLeft: "50px",
-  color: "white",
+	background: "#82C7AF",
+	padding: "10px",
+	width: "max-content",
+	marginLeft: "50px",
+	color: "white",
 };
 
 export const PaginatedEstablishmentsTable = () => {
-  const [error, setError] =
-    useState<{ message: string; [key: string]: string }>();
-  const [establishments, setEstablishments] = useState<
-    { [key: string]: string }[]
-  >([]);
-  const [pageNum, setPageNum] = useState(1);
-  const [pageCount] = useState(100);
+	const [error, setError] = useState<{ message: string; [key: string]: string }>();
+	const [establishments, setEstablishments] = useState<{ [key: string]: string }[]>([]);
+	const [pageNum, setPageNum] = useState(1);
+	const [pageCount] = useState(100);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result?.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		getEstablishmentRatings(pageNum).then(
+			(result) => {
+				setEstablishments(result?.establishments);
+				setLoading(false);
+			},
+			(error) => {
+				setError(error);
+			}
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  async function handlePreviousPage() {
-    pageNum > 1 && setPageNum(pageNum - 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
-  }
+	async function handlePreviousPage() {
+		pageNum > 1 && setPageNum(pageNum - 1);
+		setLoading(true);
+		getEstablishmentRatings(pageNum).then(
+			(result) => {
+				setEstablishments(result.establishments);
+				setLoading(false);
+			},
+			(error) => {
+				setError(error);
+			}
+		);
+	}
 
-  async function handleNextPage() {
-    pageNum < pageCount && setPageNum(pageNum + 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
-  }
+	async function handleNextPage() {
+		pageNum < pageCount && setPageNum(pageNum + 1);
+		setLoading(true);
+		getEstablishmentRatings(pageNum).then(
+			(result) => {
+				setEstablishments(result.establishments);
+				setLoading(false);
+			},
+			(error) => {
+				setError(error);
+			}
+		);
+	}
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else {
-    return (
-      <div style={tableStyle}>
-        <h2>Food Hygiene Ratings</h2>
-        <EstablishmentsTable establishments={establishments} />
-        <EstablishmentsTableNavigation
-          pageNum={pageNum}
-          pageCount={pageCount}
-          onPreviousPage={handlePreviousPage}
-          onNextPage={handleNextPage}
-        />
-      </div>
-    );
-  }
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else {
+		return (
+			<div style={tableStyle}>
+				<h2>Food Hygiene Ratings</h2>
+				<EstablishmentsTable establishments={establishments} loading={loading} />
+				<EstablishmentsTableNavigation
+					pageNum={pageNum}
+					pageCount={pageCount}
+					onPreviousPage={handlePreviousPage}
+					onNextPage={handleNextPage}
+				/>
+			</div>
+		);
+	}
 };
